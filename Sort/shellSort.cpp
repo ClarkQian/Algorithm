cfc7d0fde682e040bbbf035e82873095093ad9d2
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2018-12-04 00:28:47 +0800

    Create shellSort.cpp

---
+++ Sort/shellSort.cpp
@@ -0,0 +1,32 @@
+template<typename T>
+void shellSort(T arr[], int n){
+
+    // 计算 increment sequence: 1, 4, 13, 40, 121, 364, 1093...
+    int h = 1;
+    while( h < n/3 )
+        h = 3 * h + 1;
+
+    while( h >= 1 ){
+
+        // h-sort the array
+        for( int i = h ; i < n ; i ++ ){
+
+            // 对 arr[i], arr[i-h], arr[i-2*h], arr[i-3*h]... 使用插入排序
+            T e = arr[i];
+            int j;
+
+            //!!!!here is  j= i !!!!!!!!
+            //h is the interval???
+            // j - h >= 0;
+            //when h = 1, it is the same as the insertion sort.
+            for( j = i ; j >= h && e < arr[j-h] ; j -= h )
+                arr[j] = arr[j-h];
+            arr[j] = e;
+        }
+        //about above thing. when the i is near to h the child loop is only swap at most 1 time
+        //but when the size is long enough... i is much bigger than h. 
+        //at that time the child loop will have  arr[i],arr[i - h], arr[i - 2*h].....
+
+        h /= 3;
+    }
+}
