64c52a12b27c2950cbfd184597fd1e6372d4b616
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2018-12-20 23:00:56 +0800

    Update DfsComponent.cpp

--- Graph Theory/DfsComponent.cpp
+++ Graph Theory/DfsComponent.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
-
+//version 1
 //deep first search
 //here deep means if one vertex's connecter is unvisited -> this vertex -> this vertex's connecr(deep and deep)
 //so it just like tree, we can use recursion.
+
+
+//version 2
+// add id to know whether two vertexes is connected by id[] just like union find.
+
+
 
 template <typename Graph>
 class Component{
@@ -11,13 +17,16 @@ private:
     Graph &G;
     bool *visited; //wheather it is visited
     int ccount; // component count
+    int *id; //unionfind
 
 public:
     Component(Graph &graph): G(graph){
         visited = new bool[G.V()];
+        id = new int[G.V()];
         ccount = 0;
         for ( int i = 0; i < G.V(); i++){
             visited[i] = false;
+            id[i] = -1;
         }
 
         for( int i = 0; i < G.V(); i++){
@@ -35,7 +44,7 @@ private:
     void dfs (int v){
         //must write here otherwise it will ignore the start element.
         visited[v] = true;
-
+        id[v] = ccount;
         //I don't know whether it is a class or valable
         typename Graph::adjIterator adj(G, V);
 
