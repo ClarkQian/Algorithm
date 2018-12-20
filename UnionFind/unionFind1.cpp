8ad04e073b7887865c64adb8589e920b266cc562
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2018-12-20 14:48:24 +0800

    explain why it is need to improve.

--- UnionFind/unionFind1.cpp
+++ UnionFind/unionFind1.cpp
@@ -13,6 +13,8 @@ public:
 		for (int i = 0; i < n; i++)
 			id[i] = i;
 	}
+	
+	//o(1) -> great.
 	int find(int p) {
 		return id[p];
 	}
@@ -20,7 +22,10 @@ public:
 	bool isConnected(int p, int q) {
 		return find(p) == find(q);
 	}
-
+	
+	//o(n) -> no need to change so many indexes.
+	// if I can change it to only change one index?
+	// to realize it, we can use the the tree struct instead of a common union.
 	void unionElements(int p, int q) {
 		int pID = find(p);
 		int qID = find(q);
