b6be0904aff836c1f9f6bf5ca57b9eb7bfc90260
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2018-12-29 00:51:20 +0800

    Create HeapSort.cpp

---
+++ Heap/HeapSort.cpp
@@ -0,0 +1,35 @@
+//MUST KNOW:
+//when sort by using heap, you only need to use is shift down, no need to use shift up
+//shift up is used in insert process.
+
+
+//k is the index of the one need to be shifted down
+//n is the size of the array
+void shiftDown(int* arr, int n, int k){
+
+	while(2*k+1 <= n-1){
+		int j = 2*k+1;
+		if(j+1 <= n-1 && arr[j+1] > arr[j])
+			j = j+1;
+		if(arr[k] > arr[j])
+			break;
+		else {
+			swap(arr[k], arr[j]);
+			k = j;
+		}
+	}
+}
+
+
+
+
+//index strat form 0
+void heapSort(int* arr, int n){
+	for(int i = (n-1-1)/2; i >= 0; i--){
+		shiftDown(arr, n, i);
+	}
+	for(int i = n - 1; i >= 1; i--){
+		swap(arr[0], arr[i]);
+		shiftDown(arr, i, 0);
+	}
+}
